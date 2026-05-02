15f4791af4529724dae6ef70a91eb6499c294551
yuvrajsinh jadeja <yuvrajsinhjadeja25023@gmail.com>
2026-05-02 11:57:22 +0530

    Refactor index.js to use Express and EJS

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-const {faker}=require("@faker-js/Faker");
+const express=require("express");
+const app=express();
+const {faker}=require("@faker-js/faker");
 const mysql=require("mysql2");
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+let port=8080;
 
-let connection=mysql.createConnection({
+const connection=mysql.createConnection({
     host:"localhost",
     user:"root",
     database:"student",
     password:"abc@123"
 })
 
-let getrandomUser=()=>{
-    return[
-        faker.string.uuid(),
-        faker.internet.username(),
-        faker.internet.email(),
-        faker.internet.password()
-    ]
-}
 
-let q="INSERT INTO user(id,name,email,password)values ?";
+app.listen(port,()=>{
+    console.log(`your port ${port} is working now`);
+})
 
-let data=[];
+app.get("/",(req,res)=>{
 
-for(let i=1;i<=100;i++){
-    data.push(getrandomUser());
-}
+    let q="select count(*) from user"
 
-try{
-    connection.query(q,[data],(err,result)=>{
-        if(err) throw err;
-        console.log(result);
-    })
-}catch(err){
-    console.log(err);
-}
+    try{
+        connection.query(q,(err,result)=>{
+            if(err)throw err;
+            let count=result[0]["count(*)"];
+            res.render("home.ejs",{count});
+        })
+    }catch(err){
+        console.log(err);
+        res.send("somthing not found");
+    }
+})
+
+// let  getRandomeUser=()=>{
+//     return[
+//         faker.string.uuid(),
+//         faker.internet.username(),
+//         faker.internet.email(),
+//         faker.internet.password()
+//     ]
+// }
+
+// let q="insert into user(id,name,email,password)values ?";
+
+
+// let data=[];
+
+// for(let i=1;i<=100;i++){
+//     data.push(getRandomeUser());
+// }
+// try{
+//     connection.query(q,[data],(err,result)=>{
+//         if(err)throw err;
+//         console.log(result);
+//     })
+
+// }catch(err){
+//     console.log(err);
+// }
