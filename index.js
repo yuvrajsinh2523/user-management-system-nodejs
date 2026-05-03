9e85621ff0045967acde25fc6fae935ebee3e130
yuvrajsinh jadeja <yuvrajsinhjadeja25023@gmail.com>
2026-05-03 11:53:26 +0530

    Refactor user management routes and add UUID support

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require("express");
 const app=express();
+const path=require("path");
+const port=8080;
+const methodoverride=require("method-override");
+const {v4:uuidv4}=require("uuid");
+
+
 const {faker}=require("@faker-js/faker");
 const mysql=require("mysql2");
-const path=require("path");
-let port=8080;
-const methodoverride=require("method-override");
-app.use(methodoverride("_method"));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
 app.use(express.urlencoded({extended:true}));
+app.use(methodoverride("_method"));
+
 const connection=mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -14,22 +22,32 @@ const connection=mysql.createConnection({
     password:"abc@123"
 })
 
-let getRandomUser=()=>{
-    return[
-        faker.string.uuid(),
-        faker.internet.username(),
-        faker.internet.email(),
-        faker.internet.password()
-    ]
-}
-
 app.listen(port,()=>{
-    console.log(`your port number ${port} is working`);
+    console.log("your port is working")
 })
 
+app.get("/user/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+app.post("/user",(req,res)=>{
+    let {name,email,password}=req.body;
+    let id=uuidv4();
+    let q=`insert into user(id,name,email,password)values (?,?,?,?)`;
+    try{
+        connection.query(q,[id,name,email,password],(err,result)=>{
+            if(err)throw err;
+            res.redirect("/user");
+        })
+
+    }catch(err){
+        console.log(err);
+    }
+})
 
 app.get("/",(req,res)=>{
-    let q="select count(*) from user";
+    let q=`select count(*) from user`;
+
     try{
         connection.query(q,(err,result)=>{
             if(err)throw err;
@@ -41,19 +59,17 @@ app.get("/",(req,res)=>{
     }
 })
 
-
 app.get("/user",(req,res)=>{
     let q=`select * from user`;
     try{
         connection.query(q,(err,user)=>{
-            if(err)throw err;
-            res.render("showuser.ejs",{user})
+        if(err)throw err;
+        res.render("showuser.ejs",{user});
         })
-    }catch(er)
-    {
+    }catch(er){
         console.log(er);
     }
-});
+})
 
 app.get("/user/:id/edit",(req,res)=>{
     let {id}=req.params;
@@ -64,49 +80,72 @@ app.get("/user/:id/edit",(req,res)=>{
             if(err)throw err;
             let user=result[0];
             res.render("edit.ejs",{user});
-        })
+        });
 
     }catch(err){
         console.log(err);
-
     }
-});
 
-
+})
 
 app.patch("/user/:id",(req,res)=>{
     let {id}=req.params;
-    let {password:formpass,username:newusername}=req.body;
-
+    let {username:newUserName,password:formpass}=req.body;
     let q=`select * from user where id='${id}'`;
     try{
-        connection.query(q,(err,result)=>{
-            if(err)throw err;
-            
-            let user=result[0];
-            if(formpass!=user.password){
-                res.send("wrong password");
-            }else{
-                let q2=`update user set name='${newusername}' where id='${id}'`;
-
-                connection.query(q2,(err,result)=>{
-                    if(err)throw err;
-                    res.redirect("/user");
-                })
+        connection.query(q,(err,request)=>{
+            if(err) throw err;
+            else{
+                let q2=`update into user set name='${newUserName}' where password='${formpass}'`;
+                res.redirect("/user");
             }
         })
     }catch(err){
         console.log(err);
     }
 })
-// let q="insert into user(id,name,email,password)values ?";
 
-// let data=[];
+app.delete("/user/:id",(req,res)=>{
+    let {id}=req.params;
+    let q=`delete from user where id=?`;
 
-// for(let i=0;i<=100;i++){
-//     data.push(getRandomUser());
+    try{
+        connection.query(q,[id],(err,result)=>{
+            if(err)throw err;
+            res.redirect("/user");
+        })
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// const getRandomUser=()=>{
+//     return[
+//         faker.string.uuid(),
+//         faker.internet.username(),
+//         faker.internet.email(),
+//         faker.internet.password()
+//     ]
 // }
 
+
+// let q=`insert into user(id,name,email,password)values ?`;
+// let data=[];
+
+// for(let i=1;i<=100;i++){
+//     data.push(getRandomUser());
+// }
 // try{
 //     connection.query(q,[data],(err,result)=>{
 //         if(err)throw err;
