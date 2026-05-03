de69db5347aba6f536b36f1a0c86f3e0802fdc40
yuvrajsinh jadeja <yuvrajsinhjadeja25023@gmail.com>
2026-05-03 12:07:21 +0530

    Refactor index.js for user management functionality

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const mysql=require("mysql2");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodoverride("_method"));
